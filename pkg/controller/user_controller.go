9650d317de4618dff1515f7c7c7067e12ccda823
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-12-13 18:47:25 +0700

    refactor(response): refactor the error response functions to be reusable

--- pkg/controller/user_controller.go
+++ pkg/controller/user_controller.go
@@ -1,72 +1,69 @@
 package controller
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sylphritz/go-api-server/pkg/db/schema"
 	"github.com/sylphritz/go-api-server/pkg/db/service"
 	"github.com/sylphritz/go-api-server/pkg/response"
 	"github.com/sylphritz/go-api-server/pkg/util"
-	"gorm.io/gorm"
 )
 
 type UserResponse struct {
 	Data *schema.User `json:"data"`
 }
 
+func abortRequiredFieldError(c *gin.Context, fieldName string) {
+	response.RespondWithError(c, response.NewBadRequestError(fmt.Sprintf("%v is required", fieldName)))
+}
+
+func getRequiredFields(c *gin.Context, fields []string) (map[string]string, bool) {
+	data := map[string]string{}
+
+	for _, field := range fields {
+		value, found := c.GetPostForm(field)
+		if !found {
+			abortRequiredFieldError(c, field)
+			return data, false
+		}
+
+		data[field] = value
+	}
+
+	return data, true
+}
+
+func isDuplicateEmail(err error) bool {
+	return strings.Contains(err.Error(), "duplicate key")
+}
+
 func RegisterUser(c *gin.Context) {
-	email, found := c.GetPostForm("email")
-	if !found {
-		c.AbortWithStatusJSON(http.StatusBadRequest, response.ErrorResponse{
-			Status:  http.StatusBadRequest,
-			Message: fmt.Sprintf("%v is required", email),
-		})
+	data, ok := getRequiredFields(c, []string{"email", "password"})
+	if !ok {
 		return
 	}
 
-	password, found := c.GetPostForm("password")
-	if !found {
-		c.AbortWithStatusJSON(http.StatusBadRequest, response.ErrorResponse{
-			Status:  http.StatusBadRequest,
-			Message: fmt.Sprintf("%v is required", password),
-		})
-		return
-	}
-
-	fmt.Println("Incoming user registration request:")
-	fmt.Println("- email:", email)
-	fmt.Println("- password:", password)
-
-	hashedPassword, err := util.GenerateHashedPassword(password)
+	hashedPassword, err := util.GenerateHashedPassword(data["password"])
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, response.ErrorResponse{
-			Status:  http.StatusInternalServerError,
-			Message: "Error generating password hash",
-		})
+		response.RespondWithError(c, response.NewInternalServerError())
 		return
 	}
 
 	user := schema.User{
-		Email:    email,
+		Email:    data["email"],
 		Password: hashedPassword,
 	}
 
 	err = service.CreateUser(&user)
 	if err != nil {
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
-			c.AbortWithStatusJSON(http.StatusConflict, response.ErrorResponse{
-				Status:  http.StatusConflict,
-				Message: "Email already exists",
-			})
+		if isDuplicateEmail(err) {
+			response.RespondWithError(c, response.NewConflictError("Email already exists."))
 			return
 		} else {
-			c.AbortWithStatusJSON(http.StatusInternalServerError, response.ErrorResponse{
-				Status:  http.StatusInternalServerError,
-				Message: "Internal server error",
-			})
+			response.RespondWithError(c, response.NewInternalServerError())
 			return
 		}
 	}
--- pkg/response/error.go
+++ pkg/response/error.go
@@ -1,6 +1,34 @@
 package response
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type ErrorResponse struct {
-	Status  int    `json:"status"`
+	Code    int    `json:"code"`
 	Message string `json:"message"`
+}
+
+const InternalServerErrorMessage = "Internal server error."
+
+func NewErrorResponse(code int, message string) ErrorResponse {
+	return ErrorResponse{code, message}
+}
+
+func NewInternalServerError() ErrorResponse {
+	return NewErrorResponse(http.StatusInternalServerError, InternalServerErrorMessage)
+}
+
+func NewBadRequestError(message string) ErrorResponse {
+	return NewErrorResponse(http.StatusBadRequest, message)
+}
+
+func NewConflictError(message string) ErrorResponse {
+	return NewErrorResponse(http.StatusConflict, message)
+}
+
+func RespondWithError(c *gin.Context, err ErrorResponse) {
+	c.AbortWithStatusJSON(err.Code, err)
 }
