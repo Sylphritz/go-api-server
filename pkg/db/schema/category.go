55853b526e6b52a5c467f059533dd5e1cb7b9177
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2024-03-01 13:48:30 +0700

    fix(schema): change the unique index of category from title to slug

--- pkg/db/schema/category.go
+++ pkg/db/schema/category.go
@@ -2,8 +2,9 @@ package schema
 
 type Category struct {
 	BaseEntity
-	Name        string `gorm:"uniqueIndex:idx_category_name_blog_id;not null" json:"name"`
+	Name        string `gorm:"not null" json:"name"`
+	Slug        string `gorm:"uniqueIndex:idx_category_slug_blog_id;not null" json:"slug"`
 	Description string `json:"description"`
 	Posts       []Post `json:"-"`
-	BlogID      uint   `gorm:"uniqueIndex:idx_category_name_blog_id;not null" json:"blogId"`
+	BlogID      uint   `gorm:"uniqueIndex:idx_category_slug_blog_id;not null" json:"blogId"`
 }
